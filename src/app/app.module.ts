34af599d422b60a50a12018c886d20fefc22fe73
Joao Aires <jo.airesm@gmail.com>
2018-10-02 19:25:23 +0200

    Home component and routing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only (was true by default)
+    ),
     BrowserModule
   ],
   providers: [],
